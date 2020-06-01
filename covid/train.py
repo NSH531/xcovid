634f6501649d6136a91560c6eed29bb7e1b405c3
netanel stern <nsh531@gmail.com>
2020-06-01 23:17:10 +0300

    add pointer

--- covid/train.py
+++ covid/train.py
@@ -15,17 +15,20 @@ from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 import glob
 
 class TrainCheXNet:
-    def kullback_leibler_divergence(y_true, y_pred):
-
-    def kullback_leibler_divergence(y_true,y_pred):
+    def kullback_leibler_divergence(y_true):
         from sklearn import svm,datasets
-        from keras import backend as K
+        y_pred=None
+        
         from numpy import array
-        import numpy as np
-        q=0.0
-        for i in y_pred:
-            q=q+(y_true[i]*np.log(y_true[i]/y_pred[i])   
-        return q
+
+        if(y_pred==None):
+           y_pred=array(224,224)
+           for i in y_pred:
+                y_pred.append(random.random())
+        
+        y_true = K.clip(y_true, K.epsilon(), 1)
+        y_pred = K.clip(y_pred, K.epsilon(), 1)
+        return K.sum(y_true * K.log(y_true / y_pred), axis=-1)
 
     def __init__(self):
         self.input_size = 224
@@ -81,7 +84,7 @@ class TrainCheXNet:
         self.model = Model(inputs=input, outputs=x)
 
         # Note: default learning rate of 'adam' is 0.001 as required by the paper
-        self.model.compile(optimizer='adam',  loss=TrainCheXNet.kullback_leibler_divergence(self.input,x))
+        self.model.compile(optimizer='adam',  loss=TrainCheXNet.kullback_leibler_divergence(self.input))
         return self.model
 
     @staticmethod

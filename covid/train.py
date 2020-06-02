b2a4e758292200a56b5661cb0e0ba3d88112ce42
netanel stern <nsh531@gmail.com>
2020-06-02 14:36:38 +0300

    migration loss  function from pytorch

--- covid/train.py
+++ covid/train.py
@@ -13,7 +13,7 @@ from keras.layers import Dense, Input, GlobalAveragePooling2D
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 import glob
-
+import BCEWithLogitsLoss
 class TrainCheXNet:
 
 
@@ -58,7 +58,11 @@ class TrainCheXNet:
 
         self.train_steps = (sum(cls_cnts) // self.batch_size) + 1
         self.val_steps = (val_img_cnt // self.val_batch_size) + 1
-
+    def lossA(y_true,y_pred):
+        input=y_true
+        output=y_pred
+        loss1=BCEWithLogitsLoss()
+        return loss1(input,output)
     def get_model(self):
         # DenseNet121 expects number of channels to be 3
         input = Input(shape=(self.input_size, self.input_size, 3))
@@ -71,7 +75,7 @@ class TrainCheXNet:
         self.model = Model(inputs=input, outputs=x)
 
         # Note: default learning rate of 'adam' is 0.001 as required by the paper
-        self.model.compile(optimizer='adam',  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True))
+        self.model.compile(optimizer='adam',  loss=lossA)
         return self.model
 
     @staticmethod

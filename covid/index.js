1ca94536fe9df0b2dfe4d5435729fd37c89d363f
netanel stern <nsh531@gmail.com>
2020-06-06 21:54:44 +0300

    Update index.js

--- covid/index.js
+++ covid/index.js
@@ -1,10 +1,11 @@
 const server=require('node-http-server');
+const spawn = require("child_process").spawn;
+const pythonProcess = spawn('python',["test.py"]);
  
 server.deploy(
     {
-        port:80,
-        root:'~/covid/'
+        port:80
     },
-   console.log( `Server on port ${server.config.port} is now up`)
+   console.log( `Server on port ${server.config.port} is now up`),console.log(pythonProcess)
 );
- 
+
